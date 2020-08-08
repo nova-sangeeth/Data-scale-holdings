14f2e32a81e0f771fb1f1524afd03218ff002dc8
nova-sangeeth <novasangeeth@outlook.com>
2020-08-08 21:54:07 +0530

    added the choices required for profile model

---
+++ account/choices.py
@@ -0,0 +1,12 @@
+gender_choice = (
+    ("male", "Male"),
+    ("female", "Female"),
+    ("other", "Other"),
+)
+user_choice = (
+    ("administrator", "administrator"),
+    ("professor", "professor"),
+    ("Lab Tech", "Lab Tech"),
+    ("Normal_user", "Normal_user"),
+)
+
