4dd8d9c4b3c07015df6e9d9c105a4d07e8339c80
nova-sangeeth <novasangeeth@outlook.com>
2020-08-09 22:00:00 +0530

    added new forms for personal details; user address; and user profile

---
+++ accounts/forms.py
@@ -0,0 +1,23 @@
+from django import forms
+from .models import UserProfile, personal_information, Address
+
+
+class UserProfileForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = "__all__"
+        exclude = ("user",)
+
+
+class Personal_details_form(forms.ModelForm):
+    class Meta:
+        model = personal_information
+        fields = "__all__"
+        exclude = ("user",)
+
+
+class User_address_form(forms.ModelForm):
+    class Meta:
+        model = Address
+        fields = "__all__"
+        exclude = ("user",)
