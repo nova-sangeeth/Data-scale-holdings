5f14b44732a07ba08764679ed6c41e594c42859b
nova-sangeeth <novasangeeth@outlook.com>
2020-08-09 22:04:01 +0530

    added choices for models like religion , blood group and nationality.

---
+++ accounts/choices.py
@@ -0,0 +1,33 @@
+gender_choice = (
+    ("male", "Male"),
+    ("female", "Female"),
+    ("other", "Other"),
+)
+user_choice = (
+    ("administrator", "administrator"),
+    ("professor", "professor"),
+    ("Lab Tech", "Lab Tech"),
+    ("Normal_user", "Normal_user"),
+)
+religion_choice = (
+    ("Hindu", "Hindu"),
+    ("Muslim", "Muslim"),
+    ("Christian", "Christian"),
+    ("others", "others"),
+)
+nationality_choice = (
+    ("Indian", "Indian"),
+    ("American", "American"),
+    ("Chinese", "Chinese"),
+    ("Russian", "Russian"),
+)
+user_bloodgroup_choice = (
+    ("a+", "A+"),
+    ("o+", "O+"),
+    ("b+", "B+"),
+    ("ab+", "AB+"),
+    ("a-", "A-"),
+    ("o-", "O-"),
+    ("b-", "B-"),
+    ("ab-", "AB-"),
+)
