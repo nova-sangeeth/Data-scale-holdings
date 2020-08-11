b8807a8d46427be622a5b1c664effc00e0a13b54
nova-sangeeth <novasangeeth@outlook.com>
2020-08-12 00:05:16 +0530

    added new models to the admin panel

--- accounts/admin.py
+++ accounts/admin.py
@@ -5,8 +5,10 @@ from .models import (
     UserProfile,
     Address,
     personal_information,
+    educational_information,
 )
 
 admin.site.register(UserProfile)
 admin.site.register(Address)
 admin.site.register(personal_information)
+admin.site.register(educational_information)
