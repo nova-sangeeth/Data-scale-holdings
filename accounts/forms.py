ae3c15365bc8a6d0a2ee70576921cf3d4ac96661
nova-sangeeth <novasangeeth@outlook.com>
2020-08-12 00:07:25 +0530

    Added new Modelform to the application.

--- accounts/forms.py
+++ accounts/forms.py
@@ -1,5 +1,10 @@
 from django import forms
-from .models import UserProfile, personal_information, Address
+from .models import (
+    UserProfile,
+    personal_information,
+    Address,
+    educational_information,
+)
 
 
 class UserProfileForm(forms.ModelForm):
@@ -21,3 +26,11 @@ class User_address_form(forms.ModelForm):
         model = Address
         fields = "__all__"
         exclude = ("user",)
+
+
+class educational_info_form(forms.ModelForm):
+    class Meta:
+        model = educational_information
+        fields = "__all__"
+        exclude = ("user",)
+
