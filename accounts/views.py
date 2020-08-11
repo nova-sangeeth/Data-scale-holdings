31c598cc2781183bcdede094faabeeb954daf31a
nova-sangeeth <novasangeeth@outlook.com>
2020-08-12 00:09:24 +0530

    added a new view with all the four forms for the registration.

--- accounts/views.py
+++ accounts/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
-from .models import UserProfile
-from .forms import UserProfileForm
+from .models import UserProfile, educational_information, Address, personal_information
+from .forms import (
+    UserProfileForm,
+    educational_info_form,
+    User_address_form,
+    Personal_details_form,
+)
 
 from django.contrib.auth.models import User
 
@@ -20,4 +25,38 @@ def update_profile(request):
         if form.is_valid():
             forms.save()
     return render(request, "update_profile.html", {"form": form})
+
+
+def registration(request):
+    form_1 = UserProfileForm(request.POST)
+    form_2 = educational_info_form(request.POST)
+    form_3 = User_address_form(request.POST)
+    form_4 = Personal_details_form(request.POST)
+    if request.method == "POST":
+        user = User.objects.get(user=request.user)
+        profile = UserProfile(user=user)
+        education = educational_information(user=user)
+        address = Address(user=user)
+        personal = personal_information(user=user)
+        form_1 = UserProfileForm(request.POST)
+        form_2 = educational_info_form(request.POST)
+        form_3 = User_address_form(request.POST)
+        form_4 = Personal_details_form(request.POST)
+        if (
+            form_1.is_valid()
+            and form_2.is_valid()
+            and form_3.is_valid()
+            and form_4.is_valid()
+            and form_5.is_valid()
+        ):
+            form_1.save()
+            form_2.save()
+            form_3.save()
+            form_4.save()
+            form_5.save()
+    return render(
+        request,
+        "profile.html",
+        {"form_1": form_1, "form_2": form_2, "form_3": form_3, "form_4": form_4,},
+    )
 
