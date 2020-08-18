7a0b6c03b0f07e6f399a5be81e7ea9312783f553
nova-sangeeth <novasangeeth@outlook.com>
2020-08-18 15:29:50 +0530

    added str method to stringify the user object form showing __str__ returned non-string (type User) error

--- accounts/models.py
+++ accounts/models.py
@@ -23,7 +23,7 @@ class UserProfile(models.Model):
     user_type = models.CharField(max_length=16, choices=user_choice, null=True)
 
     def __str__(self):
-        return self.name
+        return str(self.name)
 
 
 class personal_information(models.Model):
