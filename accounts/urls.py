c1601767c5a012dca9cf43acbe16da6649e45fdb
nova-sangeeth <novasangeeth@outlook.com>
2020-08-25 23:12:41 +0530

    added a new url path for the index page

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url, include
 
-from .views import registration, profile
+from .views import registration, profile, index
 
 app_name = "accounts"
 
 urlpatterns = [
     url(r"^profile_reg$", registration, name="registration"),
-    url(r"^$", profile, name="profile"),
+    url(r"^profile$", profile, name="profile"),
+    url(r"^$", index, name="index"),
 ]
